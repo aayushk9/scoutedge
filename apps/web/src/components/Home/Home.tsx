b1b71f56a91de2c802813f65a0431c570043a4b2
Aayush <ayushkokate1@gmail.com>
2025-09-17 23:55:04 +0530

    feat(ui): center divs

--- apps/web/src/components/Home/Home.tsx
+++ apps/web/src/components/Home/Home.tsx
@@ -1,7 +1,7 @@
 import styles from './Home.module.css'
 import { Navbar } from '../Navbar/Navbar'
 import { Sidebar } from '../Sidebar/Sidebar'
-import { InputBox } from '../InputBOX/InputBox'
+import { InputBox } from '../InputBox/InputBox'
 
 export function Home () {
     return (
@@ -11,13 +11,13 @@ export function Home () {
           </div>
           <div>
              <Sidebar/>
-             <main>
+             <main className={styles.main}>
                 <h1>Welcome to Scoutedge</h1>
                 <InputBox/>
-             </main>
              <footer>
-                <p>Dawn can help you with thinhs like...</p>
+                <p>Dawn can help you with things like...</p>
              </footer>
+             </main>
           </div>
         </div>
     )
